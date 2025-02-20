fc3b8b1821d179fdd37eede6330fa29472b7a659
mffree <matthewfreeman63@gmail.com>
2025-02-20 03:38:09 +0000

    directors#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index"}) # standard to name the controller after the table and name the action index since it pulls the records from the table
 end
