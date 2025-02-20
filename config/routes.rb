f793171b0476e779fef41788741f72e6d82f0fd6
mffree <matthewfreeman63@gmail.com>
2025-02-20 04:08:05 +0000

    filmography

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  
   get("/directors", { :controller => "directors", :action => "index"}) # standard to name the controller after the table and name the action index since it pulls the records from the table
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show" }) # dynamic route for the director details
 end
